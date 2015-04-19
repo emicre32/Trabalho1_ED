16c80336e4620e2282247c20b1401341c70db723
Smykke <Smykke@users.noreply.github.com>
2015-04-18 22:49:52 -0300

    Primeiras estruturas

--- funcoes.h
+++ funcoes.h
@@ -1,1 +1,13 @@
+typedef struct rua_bairro {
+	int id;
+	char nome;
+	struct tipoRB * prox;
+} TipoRB;
 
+typedef struct moradias {
+	int id_consumidor;
+	int id_casa;
+	int consumo;
+	char consumidor;
+	struct moradias * prox;
+} Casa;
