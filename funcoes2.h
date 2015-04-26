8a714055940a20e6b8d25a203acdf30dd667a535
emicre32 <emilia.cremasco@gmail.com>
2015-04-26 01:02:11 -0300

    Funções de casa funcionando. Problema para ler comando bairro

--- funcoes2.h
+++ funcoes2.h
@@ -31,7 +31,7 @@ typedef struct Casa
     int id_casa;
     float consumo;
     char *nome_consumidor;
-    struct house *proxc;
+    struct Casa *proxc;
 } house;
 
 
@@ -110,8 +110,15 @@ nhood * procura_bairro(nhood * head_bairro, int idBairro)
     nhood *aux = head_bairro->proxb;
     while(idBairro != aux->id_bairro)
     {
-        if (aux->proxb == NULL) return NULL;
-        aux = aux->proxb;
+        if (aux->proxb == NULL) 
+        {  
+          return NULL;
+          break;
+        }  
+        else
+        {
+          aux = aux->proxb;
+        }
     }
     return aux;
 }
@@ -275,6 +282,8 @@ street *insere_rua(nhood * head_bairro, int idBairro, int id, char *nome)
 
 void retira_rua(nhood *head_bairro, int idBairro, int id)
 {
+    
+    
     nhood *bairro = procura_bairro(head_bairro, idBairro);
     if(bairro != NULL)
     { 
@@ -295,9 +304,9 @@ void retira_rua(nhood *head_bairro, int idBairro, int id)
 
               if(proxima->id_rua == id)
               {
-                anterior = proxima->proxr;
-                free(proxima);
-                printf("Rua removida com sucesso. Bairro id: %i Rua id: %i\n", idBairro, id);
+                //anterior = proxima->proxr;
+                //free(proxima);
+                //printf("Rua removida com sucesso. Bairro id: %i Rua id: %i\n", idBairro, id);
                 break;
               }
               else if (proxima->proxr == NULL)
@@ -309,12 +318,320 @@ void retira_rua(nhood *head_bairro, int idBairro, int id)
          
           if(proxima->id_rua == id)
           {
+            
             anterior = proxima->proxr;
+            proxima->id_rua = 0;
             free(proxima);
             printf("Rua removida com sucesso. Bairro id: %i Rua id: %i\n", idBairro, id);
+
           }
 
         }
     }
     else printf("Bairro vazio ou rua com ID=%i não encontrado.\nERRO: Rua nao removida. Bairro id: %i Rua id: %i\n", id, idBairro, id);
 } 
+
+
+//FIM DE FUNCOES PARA RUA--------------------------------------------------------------------------------------//
+
+
+
+//FUNCOES PARA CASA--------------------------------------------------------------------------------------------//
+house *aloca_casa(int idbairro, int idrua, int idconsumidor, int casa, float consumo, char *consumidor)
+{
+    house *novacasa = (house*) malloc(sizeof(struct Casa));
+    if(!novacasa)
+    {
+        printf("ERROR: NAO HA MEMORIA DISPONIVEL");
+        exit(1);
+    }
+    else
+    {
+        novacasa->id_consumidor = idconsumidor;
+        novacasa->nome_consumidor = (char*)malloc(strlen(consumidor)+1*sizeof(char));
+        strcpy(novacasa->nome_consumidor, consumidor);
+        novacasa->consumo = consumo;
+        novacasa->proxc = NULL;
+        novacasa->id_casa = casa;
+        printf("Casa incluida com sucesso. Bairro id: %d Rua id: %d Casa id: %d Casa nr: %d Consumidor nome '%s'\n", idbairro, idrua, idconsumidor, casa, consumidor );
+        return novacasa;       
+    }
+    
+}
+
+street *procura_rua(nhood *head_bairro, int idRua, int idBairro)
+{
+    
+    nhood *bairro = procura_bairro(head_bairro, idBairro);
+    if( bairro != NULL)
+    {
+        street *aux = bairro->head_rua;
+        while(idRua != aux->id_rua)
+        {
+           aux = aux->proxr;
+        }
+        if(aux->proxr == NULL && aux->id_rua != idRua)
+        {
+           printf("Rua não encontrada!\n");
+           return NULL;
+        }
+        else return aux;
+    }
+    else return NULL;    
+
+}
+
+house *insereinicio_casa(nhood *head_bairro, int idbairro, int idrua, int idconsumidor, int casa, float consumo, char *consumidor)
+{
+    street *rua = procura_rua(head_bairro, idrua, idbairro);
+    if(rua != NULL)
+    {
+        house *nova = aloca_casa(idbairro, idrua, idconsumidor, casa, consumo, consumidor);
+        house *old_primeira;
+        old_primeira = rua->head_casa->proxc; //armazenando o endereço da primeira casa
+
+        rua->head_casa->proxc = nova; //colocando o bairro desejado no primeiro lugar da lista
+        nova->proxc = old_primeira;
+        return nova;
+    }
+    else return NULL;    
+}
+
+house *insere_casa(nhood *head_bairro, int idbairro, int idrua, int idconsumidor, int casa, float consumo, char *consumidor)
+{
+
+    street *rua = procura_rua(head_bairro, idrua, idbairro);
+    if(rua != NULL)
+    {
+        if(rua->head_casa == NULL)
+        {
+           house *nova = aloca_casa(idbairro, idrua, idconsumidor, casa, consumo, consumidor);
+           rua->head_casa = nova;
+           return nova;
+        } 
+        else
+        {
+           if(casa < rua->head_casa->id_casa)
+           {
+              return insereinicio_casa(head_bairro, idbairro, idrua, idconsumidor, casa, consumo, consumidor);
+           } 
+           else 
+           {   
+              house *casa_atual = rua->head_casa;
+              house *nova;
+        
+              while(casa > casa_atual->id_casa)
+              {
+                if(casa_atual->proxc == NULL) break;
+                else
+                {
+                  if(casa_atual->proxc->id_casa < casa)
+                  {
+                      house *casa_anterior;
+                      casa_anterior = casa_atual;
+                      casa_atual = casa_atual->proxc; 
+                     continue;
+                  }
+                  else break;      
+                }
+              }
+        
+              if(casa == casa_atual->id_casa)
+              {
+                  printf("Essa casa ja foi cadastrada!\n");
+                  return NULL;
+              } 
+              else 
+              {
+                 house *nova = aloca_casa(idbairro, idrua, idconsumidor, casa, consumo, consumidor);
+                 if(casa_atual->proxc == NULL) //se for para inserir o novo bairro no fim da lista
+                 {
+                    casa_atual->proxc = nova;  
+                 }
+                 else //se for para inserir no meio
+                 {
+                    house *aux;
+                    aux = casa_atual->proxc;
+                    casa_atual->proxc = nova;
+                    nova->proxc = aux;
+                 }
+                 return nova;
+             }  
+          }
+        }
+
+    }
+    else return NULL;        
+
+        
+}
+
+int *retirainicio_casa(nhood *head_bairro, int idbairro, int idrua, int idcasa)
+{
+    street *rua = procura_rua(head_bairro, idrua, idbairro);
+    if(rua->head_casa == NULL)
+    {
+        printf("Nao ha casas cadastradas nessa rua!\n");
+        return 0;
+    }
+    else
+    {
+         house *aux = rua->head_casa->proxc;
+         rua->head_casa->proxc = aux->proxc;
+         printf("Casa %d retirada com sucesso!\n", idcasa );
+         return 0;
+
+    }
+}
+
+
+void retira_casa(nhood *head_bairro, int idBairro, int idrua, int idconsumidor)
+{    
+    street *rua = procura_rua(head_bairro, idrua, idBairro);
+    if(rua != NULL)
+    { 
+        if (rua->head_casa == NULL) printf("Nao ha casas cadastradas nesta rua!\n");
+        else 
+        {
+          house *proxima;
+          house *anterior;
+
+          anterior = NULL;
+          proxima = rua->head_casa;
+
+
+          while (proxima->id_consumidor != idconsumidor)
+          {
+              anterior = proxima;
+              proxima = proxima->proxc;
+
+              if(proxima->id_consumidor == idconsumidor)
+              {
+                break;
+              }
+              else if (proxima->proxc == NULL)
+                    {
+                      printf("Consumidor com ID=%i não encontrado.\nERRO: Casa nao removida. Bairro id: %i Rua id: %i\n", idconsumidor, idBairro, idrua);
+                      break;
+                    }
+            }
+         
+          if(proxima->id_consumidor == idconsumidor)
+          {
+            
+            anterior = proxima->proxc;
+            proxima->id_consumidor = 0;
+            free(proxima);
+            printf("Casa removida com sucesso. Consumidor id: %i Bairro id: %i Rua id: %i\n", idconsumidor, idBairro, idrua);
+
+          }
+
+        }
+    }
+    else printf("Consumidor com ID=%i não encontrado.\nERRO: Casa nao removida. Bairro id: %i Rua id: %i\n", idconsumidor, idBairro, idrua);
+} 
+
+//FIM DE FUNCOES PARA CASA---------------------------------------------------------------------------------------------------------------------//
+
+
+//FUNCOES PARA MEDIR CONSUMO------------------------------------------------------------------------------------------------------------------//
+float medir_rua(nhood *head_bairro, int idbairro, int idrua)
+{
+    float consumo_rua=0;
+    street *rua = procura_rua(head_bairro, idrua, idbairro);
+    if(rua != NULL)
+    {  
+      house *aux = rua->head_casa->proxc;
+      while(aux->proxc != NULL)
+      {
+          consumo_rua += aux->consumo;
+          aux = aux->proxc;
+      }
+      return consumo_rua;
+    }
+    else return 0; 
+
+} 
+
+house *procura_casa(nhood *head_bairro, int idbairro, int idrua, int idcasa)
+{
+    street *rua = procura_rua(head_bairro, idrua, idbairro);
+    if(rua != NULL)
+    {    
+        if(rua->head_casa == NULL) 
+        {
+           printf("Casa não encontrada!\n");
+           return NULL;
+        }
+        else
+        {  
+          house *aux = rua->head_casa;
+          while(idcasa != aux->id_consumidor)
+          {
+            aux = aux->proxc;
+          }
+          if(aux->proxc == NULL && aux->id_consumidor != idcasa)
+          {
+             printf("Casa não encontrada!\n");
+             return NULL;
+          }
+          else return aux;
+        } 
+    }
+    else return NULL;    
+}
+
+
+float medir_casa(nhood *head_bairro, int idbairro, int idrua, int idcasa)
+{
+    float consumo_medir;
+    house *casa = procura_casa(head_bairro, idbairro, idrua, idcasa);
+    if( casa != NULL)
+    {
+        consumo_medir = casa->consumo;
+        return consumo_medir;
+    }
+    else return 1;    
+}
+
+int inserir_consumo(nhood *head_bairro, int idbairro, int idrua, int idcasa, float consumo)
+{
+    house *casa = procura_casa(head_bairro, idbairro, idrua, idcasa);
+    if (casa != NULL) 
+    {    
+        casa->consumo += consumo;
+        return 0;
+    }  
+    else return 1;
+}
+
+float medir_bairro(nhood *head_bairro, int idbairro)
+{
+    float consumo_medir=0;
+    nhood *bairro = procura_bairro(head_bairro, idbairro);
+    if(bairro!= NULL)
+    {
+       if(bairro->head_rua != NULL)
+       {
+          street *rua = bairro->head_rua;
+          
+          while(rua->proxr != NULL)
+          {
+             int idrua = rua->id_rua;
+             consumo_medir += medir_rua(head_bairro, idbairro, idrua );
+             rua = rua->proxr;
+          }
+          printf("O consumo total desse bairro e %f\n", consumo_medir );
+          return consumo_medir;
+       }
+       else
+       {
+        printf("Nao ha ruas nesse bairro!\n");
+        return 0;
+       }
+    }
+    else return 0;
+}
+
+
+
--- oi.c
+++ oi.c
@@ -9,13 +9,17 @@ int main() {
     // Variáveis
     FILE * mapa;
     char * comando, *descr;
-    int id1, id2;
-    //int i;
+    int id1, id2, id3, id4;
+    float id5;
     int * pid1 = NULL;
     int * pid2 = NULL;
+    int * pid3 = NULL;
+    int * pid4 = NULL;
+    float * pid5 = NULL;
+
     nhood *head_bairro;
     nhood *aux;
-
+    int answer = 1;
 
     // Alocações
     aux = (nhood*)malloc(sizeof(nhood));
@@ -23,10 +27,12 @@ int main() {
     descr = (char*)malloc(60*sizeof(char));
     pid1 = (int*)malloc(sizeof(int));
     pid2 = (int*)malloc(sizeof(int));
+    pid3 = (int*)malloc(sizeof(int));
+    pid4 = (int*)malloc(sizeof(int));
+    pid5 = (float*)malloc(sizeof(float));
 
 
     // Inicialização
-    mapa = abrirMapa();
     head_bairro = inicia_bairro(aux);
 
     //inserção do primeiro bairro (deletado no fim)
@@ -34,57 +40,138 @@ int main() {
     nhood* nulo = aloca_bairro(id1, "nulo");
     head_bairro->proxb = nulo;
 
+    //Inserção dos bairros
+    while (answer == 1){
+        printf("Digite o id e o respectivo nome dos bairros:    <id, nome>\n");
+        scanf("%i, %[^\n]", &id1, descr);
+        insere_bairro(head_bairro, id1, descr);
+        printf("Deseja adicionar mais um bairro? <1> Sim   <qualquer número> Não\n");
+        scanf("%i", &answer);
+    }
 
+
+    // Inicialização do arquivo
+    mapa = abrirMapa();
 
 
     while(!feof(mapa))
     {
-    //for(i=0; i<3; i++){
         fscanf(mapa, "%s", comando);  // rua, casa, bairro ou cidade
+       //printf("COMANDO DA VEZ: %s\n", comando);
         // -----  RUA
         if (strcmp(comando, "rua") == 0)
+        {
+            fscanf(mapa, "%s", comando); // incluir, eliminar ou medir
+            
+            if (strcmp(comando, "incluir") == 0)
             {
-                fscanf(mapa, "%s", comando); // incluir, eliminar ou medir
+                fscanf(mapa, "%i", pid1); // id bairro
+                id1 = *pid1;
+                fscanf(mapa, "%i", pid2); // id rua  
+                //fscanf(mapa, " "%[^\n]"", descr); // nome 
+                fscanf(mapa, " \"%[^\"\n]", descr); // nome
+                fgetc(mapa);
+                id2 = *pid2;
+                insere_rua(head_bairro, id1, id2, descr);                     
+            }
+
+            else if (strcmp(comando, "eliminar") == 0)
+            {
+                fscanf(mapa, "%i", pid1); // id bairro  
+                fscanf(mapa, "%i", pid2); // id rua  
+                
+                id1 = *pid1;
+                id2 = *pid2;
+
+                retira_rua(head_bairro, id1, id2);
+            }
+            else if (strcmp(comando, "medir") == 0)
+                 {
+                    fscanf(mapa, "%i", pid1); // id bairro  
+                    fscanf(mapa, "%i", pid1); // id rua 
+                    id1 = *pid1;
+                    id2 = *pid2;
+                    medir_rua(head_bairro, id1, id2);
+                 }
+                 else printf("Comando nao identificadoaqui.\n");
+        }
+        else if (strcmp(comando, "casa") == 0)
+             {
+                fscanf(mapa, "%s", comando); //incluir, consumir, medir  
                 if (strcmp(comando, "incluir") == 0)
                 {
-                    fscanf(mapa, "%i", pid1); // id bairro
-                    
+                    fscanf(mapa, "%i", pid1); //id bairro  
+                    fscanf(mapa, "%i", pid2); // id rua   
+                    fscanf(mapa, "%i", pid3); //id consumidor  
+                    fscanf(mapa, "%i", pid4); // num casa
+                    fscanf(mapa, "%f", pid5); // consumo
+                    fscanf(mapa, "%[^\n]", descr);  // nome consumidor 
                     id1 = *pid1;
-
-                    insere_bairro(head_bairro, id1, "camburi");
-                    insere_bairro(head_bairro, id1, "camburi");
-
-
-                    fscanf(mapa, "%i", pid2); // id rua  
-                    fscanf(mapa, "%[^\n]", descr); // nome 
-
                     id2 = *pid2;
-                    insere_rua(head_bairro, id1, id2, descr);                     
+                    id3 = *pid3;
+                    id4 = *pid4;
+                    id5 = *pid5;
+                    insere_casa(head_bairro, id1, id2 , id3, id4, id5, descr);
                 }
+                else if (strcmp(comando, "eliminar") == 0)
+                     {
+                         fscanf(mapa, "%i", pid1); //id bairro  
+                         fscanf(mapa, "%i", pid2); // id rua   
+                         fscanf(mapa, "%i", pid3); //id casa 
+                         id1 = *pid1;
+                         id2 = *pid2;
+                         id3 = *pid3;                     
+                         retira_casa(head_bairro, id1, id2, id3);
+                     }
+                     else if (strcmp(comando, "consumir") == 0)
+                          {
+                            fscanf(mapa, "%i", pid1); //id bairro  
+                            fscanf(mapa, "%i", pid2); // id rua   
+                            fscanf(mapa, "%i", pid3); //id casa 
+                            fscanf(mapa, "%f", pid5); //consumo
+                            id1 = *pid1;
+                            id2 = *pid2;
+                            id3 = *pid3;
+                            id5 = *pid5; //float
+                            inserir_consumo(head_bairro, id1, id2, id3, id5);
+                          }
+                         else if (strcmp(comando, "medir") == 0)
+                              {
+                                fscanf(mapa, "%i", pid1); //id bairro  
+                                fscanf(mapa, "%i", pid2); // id rua   
+                                fscanf(mapa, "%i", pid3); //id casa 
+                                id1 = *pid1;
+                                id2 = *pid2;
+                                id3 = *pid3; 
+                                medir_casa(head_bairro, id1, id2, id3);
+                              }
+                              else printf("Comando nao identificadohere.\n");
+             } 
+             else if (strcmp(comando, "bairro") == 0)
+                  {
+                    fscanf(mapa, "%s", comando); // incluir, medir
+                    if (strcmp(comando, "incluir"))
+                    {
+                      fscanf(mapa, "%i", pid1); // id  
+                      fscanf(mapa, "%[^\n]", descr); // nome
+                      // função <incluir_bairro> //
+                    }
+                    else if (strcmp(comando, "medir") == 0)
+                         {
+                            fscanf(mapa, "%i", pid1); // id bairro 
+                            // função <medir_bairro> //
+                         }
+                         else printf("Comando nao identificadohere2.\n");
+                  } 
+                  else printf("Comando nao identificadohere3.\n"); 
+    
+    }//while
 
-            }
-            //fscanf(mapa, "%s", comando);  //     rua, casa, bairro ou cidade
-            //fscanf(mapa, "%s", comando);
-            //printf("%i\n", *comando);
-            //printf("%c\n", *comando);
-        }
     return 0;
 }
             
  /*   
-                else if (strcmp(comando, "eliminar") == 0)
-                {
-                    fscanf(mapa, "%i", pid1); // id bairro  
-                    fscanf(mapa, "%i", pid1); // id rua  
-                    // função <eliminar_rua> 
-                }
-                    else if (strcmp(comando, "medir") == 0)
-                    {
-                        fscanf(mapa, "%i", pid1); // id bairro  
-                        fscanf(mapa, "%i", pid1); // id rua  
-                        //função <medir_rua> 
-                    }
-                        else printf("Comando nao identificado.\n");
+
             }
             // -----  CASA
             else if (strcmp(comando, "casa") == 0)
